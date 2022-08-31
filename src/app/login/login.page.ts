46814a71b7c4ccce5effa4956da5ffce314013f4
t-mallea <t.mallea@duocuc.cl>
2022-08-31 09:59:52 -0400

    Se implementa animacion de login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationExtras } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 
 
 @Component({
@@ -16,18 +17,31 @@ export class LoginPage implements OnInit {
     usuario:"",
     password:""
   }
-  constructor(private router: Router) { } // Se debe instanciar
+  constructor(private router: Router, public loadingController: LoadingController ) { } // Se debe instanciar
 
   ngOnInit() {
   }
   ingresar(){
+    this.presentLoadingWithOptions();
     // Se declara e instancia un elemento de tipo NavigationExtras
     let navigationExtras: NavigationExtras = {
       state: {
         user: this.user // Al estado se asignamos un objeto con clave y valor
       }
     };
+    this.loadingController.dismiss();
     this.router.navigate(['/home'],navigationExtras); // navegamos hacia el Home y enviamos información adicional
+  }
+
+  async presentLoadingWithOptions() {
+    const loading = await this.loadingController.create({
+      //spinner: null,
+      duration: 3000,
+      message: 'Iniciando sesion...',
+      //translucent: true,
+      //cssClass: 'custom-class custom-loading'
+    });
+    return await loading.present();
   }
 
 }
